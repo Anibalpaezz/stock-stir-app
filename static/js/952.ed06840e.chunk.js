0e9fd04a52c49a0ac216f794c9110fa5f722e010
Aníbal Páez Gallego <anibalpaezgallego@gmail.com>
2024-06-13 17:09:00 +0200

    Updates

---
+++ static/js/952.ed06840e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstock_and_stir=self.webpackChunkstock_and_stir||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.ed06840e.chunk.js.map
